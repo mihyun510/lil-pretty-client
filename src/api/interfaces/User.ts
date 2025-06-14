cdc046493835f0c971ed1e41e3de3f367d439e4e
kmh <mihyun510@github.com>
2025-06-14 15:12:08 +0900

    회원가입 로직 수정중

--- src/api/interfaces/User.ts
+++ src/api/interfaces/User.ts
@@ -1,9 +1,9 @@
 export interface UserRequest {
-  us_id: string;
-  us_pw: string;
-  us_nm: string;
-  us_email: string;
-  us_phone: string;
+  usId: string;
+  usPw: string;
+  usNm: string;
+  usEmail: string;
+  usPhone: string;
 }
 
 export interface UserResponse {
