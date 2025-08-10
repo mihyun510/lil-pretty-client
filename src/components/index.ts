4093eb602b233365c85dc50c18b676d3767af10b
kmh <mihyun510@github.com>
2025-08-10 11:53:02 +0900

    다이어트존detail 수정중

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export { Header, Footer, Layout } from "./layout";
 export { LoginForm, SignupForm } from "./auth";
 export { LandingForm } from "./intro";
 export { LoginModal, SignupModal, AuthDialogWrapper } from "./modals";
-export { DietMainForm, DietMasterForm } from "./diet";
+export { DietMainForm, DietMasterForm, DietDetailForm } from "./diet";
--- src/pages/diet/index.ts
+++ src/pages/diet/index.ts
@@ -1,2 +1,3 @@
 export { default as DietMainPage } from "./DietMainPage";
 export { default as DietMasterPage } from "./DietMasterPage";
+export { default as DietDetailPage } from "./DietDetailPage";
