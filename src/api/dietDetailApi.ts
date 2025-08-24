873cc6bffa2dd7b48b8109075c982b753833b6dd
kmh <mihyun510@github.com>
2025-08-24 22:31:19 +0900

    다이어트 존 디테일 수정

--- src/api/dietDetailApi.ts
+++ src/api/dietDetailApi.ts
@@ -1,6 +1,7 @@
 import { apiInstance } from "./common/axiosInstance";
 import { CommonResponse } from "@/api/interfaces/Common";
 import { MealDtlItems } from "@/api/interfaces/MealDtl";
+import { MealFavoriteItems } from "./interfaces/MealFavorite";
 
 export async function getMealDtlItems(
   mmCd: string
@@ -23,7 +24,36 @@ export async function getMealDtlItems(
       };
     }
   } catch (error) {
-    console.error("getMealsByPrice Error:", error);
+    console.error("getMealDtlItems Error:", error);
+    return {
+      ok: false,
+      message: "서버 오류 또는 네트워크 문제로 데이터를 불러오지 못했습니다.",
+    };
+  }
+}
+
+export async function getMealFavoriteItems(): Promise<
+  CommonResponse<MealFavoriteItems[]>
+> {
+  try {
+    const response = await apiInstance.post(
+      "/diet/detail/getMealFavoriteItems"
+    );
+
+    if (response.data.ok && response.data.data) {
+      return {
+        ok: response.data.ok,
+        data: response.data.data,
+        message: response.data.message,
+      };
+    } else {
+      return {
+        ok: false,
+        message: response.data.message || "데이터를 불러올 수 없습니다.",
+      };
+    }
+  } catch (error) {
+    console.error("getMealFavoriteItems Error:", error);
     return {
       ok: false,
       message: "서버 오류 또는 네트워크 문제로 데이터를 불러오지 못했습니다.",
---
+++ src/api/interfaces/MealFavorite.ts
@@ -0,0 +1,6 @@
+export interface MealFavoriteItems {
+  mm_cd: string;
+  mf_cd: string;
+  mm_title: string;
+  mm_img?: string;
+}
