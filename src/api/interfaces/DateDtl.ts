ad31a878d0a4fb2b91e8de6091f607e3b48e2988
김민정 <minjeong1513@naver.com>
2025-09-24 23:04:48 +0900

    09/24 detail interface, api, detailForm 틀 작업

---
+++ src/api/interfaces/DateDtl.ts
@@ -0,0 +1,10 @@
+export interface DateDtl {
+  dm_cd: string;
+  dd_cd: string;
+  dm_title: string;
+  dd_maxview: string;
+  dd_title: string;
+  dd_img: string;
+  dd_desc: string;
+  dd_rank: number;
+}
--- src/components/date/DateDetailForm.tsx
+++ src/components/date/DateDetailForm.tsx
@@ -1,3 +1,20 @@
+import { getDateItems } from "@/api/dateMastApi";
+import { CommonResponse } from "@/api/interfaces/Common";
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+
+const [DateDtl, SetDateDtl] = useState<DateDtl[]>([]);
+useEffect(() => {
+  const disFetch = async () => {
+    const response: CommonResponse<DateDtl[]> = await getDateItems();
+    if (response.ok && response.data) {
+      return SetDateDtl(response.data);
+    } else {
+      console.log("메롱");
+    }
+    disFetch();
+  };
+}, []);
 export default function DateDetailForm() {
-  return <div>민동이 디테일</div>;
+  return <Box>힐링 데이트편 순위</Box>;
 }
