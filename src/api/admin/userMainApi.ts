12f95b892b0eaaa95c554ada0c1600931a8434c5
mj <minjeong1513@naver.com>
2025-12-15 23:30:59 +0900

    12/15 사용자권한관리 화면 개발 (저장 보완필요)

--- src/api/admin/userMainApi.ts
+++ src/api/admin/userMainApi.ts
@@ -1,7 +1,11 @@
 import { apiInstance } from "../common/axiosInstance";
-import { CommonResponse } from "../interfaces/Common";
+import {
+  CommonResponse,
+  CUDCommonResponse,
+  CUDFailItem,
+} from "../interfaces/Common";
 import { UserRequest } from "../interfaces/AdminUser";
-
+/* ---------------- 사용자 권한 정보 조회 ---------------- */
 export async function getAdminUserItems(
   usId: string
 ): Promise<CommonResponse<UserRequest[]>> {
@@ -32,62 +36,79 @@ export async function getAdminUserItems(
     };
   }
 }
+/* ---------------- 사용자 권한 정보 단건 삭제 ---------------- */
 export async function deleteAdminUserItems(
   usId: string
-): Promise<CommonResponse<UserRequest[]>> {
+): Promise<CUDCommonResponse<CUDFailItem>> {
   try {
     const response = await apiInstance.post(
       "/admin/user/main/deleteAdminUserItems",
       {
-        usId,
+        usId, //단일 삭제
       }
     );
-    if (response.data.ok && response.data.data) {
-      return {
-        data: response.data.data,
-        ok: response.data.ok,
-        message: response.data.message,
-      };
-    } else {
-      return {
-        ok: false,
-        message: response.data.message || "데이터를 불러올 수 없습니다.",
-      };
-    }
-  } catch (error) {
-    console.error("getAdminMealItems Error:", error);
     return {
-      ok: false,
-      message: "서버 오류 또는 네트워크 문제로 데이터를 불러오지 못했습니다.",
+      successCount: response.data.successCount,
+      failCount: response.data.failCount,
+      data: response.data.data,
+      message: response.data.message,
+    };
+  } catch (error) {
+    console.error("deleteAdminUserItems Error:", error);
+    return {
+      successCount: 0,
+      failCount: 0,
+      message: "서버 오류 또는 네트워크 문제로 삭제하지 못했습니다.",
     };
   }
 }
+/* ---------------- 사용자 권한 정보 수정---------------- */
 export async function updateAdminUserItems(
   selectedUser: UserRequest
-): Promise<CommonResponse<number>> {
+): Promise<CUDCommonResponse<CUDFailItem>> {
   try {
     const response = await apiInstance.post(
       "/admin/user/main/updateAdminUserItems",
-
       selectedUser
     );
-    if (response.data.ok && response.data.data) {
-      return {
-        data: response.data.data,
-        ok: response.data.ok,
-        message: response.data.message,
-      };
-    } else {
-      return {
-        ok: false,
-        message: response.data.message || "데이터를 불러올 수 없습니다.",
-      };
-    }
-  } catch (error) {
-    console.error("getAdminMealItems Error:", error);
+
     return {
-      ok: false,
-      message: "서버 오류 또는 네트워크 문제로 데이터를 불러오지 못했습니다.",
+      successCount: response.data.successCount,
+      failCount: response.data.failCount,
+      data: response.data.data,
+      message: response.data.message,
+    };
+  } catch (error) {
+    console.error("updateAdminUserItems Error:", error);
+    return {
+      successCount: 0,
+      failCount: 0,
+      message: "서버 오류 또는 네트워크 문제로 수정하지 못했습니다.",
+    };
+  }
+}
+/* ---------------- 사용자 권한 정보 추가---------------- */
+export async function insertAdminUserItems(
+  selectedUser: UserRequest
+): Promise<CUDCommonResponse<CUDFailItem>> {
+  try {
+    const response = await apiInstance.post(
+      "/admin/user/main/insertAdminUserItems",
+      selectedUser
+    );
+
+    return {
+      successCount: response.data.successCount,
+      failCount: response.data.failCount,
+      data: response.data.data,
+      message: response.data.message,
+    };
+  } catch (error) {
+    console.error("insertAdminUserItems Error:", error);
+    return {
+      successCount: 0,
+      failCount: 0,
+      message: "서버 오류 또는 네트워크 문제로 저장하지 못했습니다.",
     };
   }
 }
